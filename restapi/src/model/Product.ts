4793ee447f67ef7c457d918b0db4e1f179711e9c
AnaOstio <anafdezostio01@gmail.com>
2022-03-05 12:17:18 +0100

    make it better

--- restapi/src/model/Product.ts
+++ restapi/src/model/Product.ts
@@ -1,43 +1,14 @@
 const mongoose = require('mongoose')
 const { model, Schema } = mongoose
 
-const producto = new Schema ({
-    codigo: {
-        type: String,
-        required: true,
-        unique: true, 
-        trim: true
+export const ProductSchema = new Schema ({
+    codigo: { type: String, required: true, unique: true,  trim: true}, 
+    categoria: { type: String, required: true, unique: true, trim: true},
+    nombre: { type: String, required: true,trim: true}, 
+    precio: { type: Number, required: true, trim: true}, 
+    descripcion: { type: String, trim: true}, 
+    stock: { type: Number,required: true, trim: true}, 
     }, 
+    {collection: 'Product'});
 
-    categoria: {
-        type: String,
-        required: true,
-        unique: true, 
-        trim: true
-    },
-
-    nombre: {
-        type: String,
-        required: true,
-        trim: true
-    }, 
-
-    precio: {
-        type: Number,
-        required: true,
-        trim: true
-    }, 
-
-    descripcion: {
-        type: String,
-        trim: true
-    }, 
-
-    stock: {
-        type: Number,
-        required: true,
-        trim: true
-    }
-});
-
-export  const products = model('Product', producto)
+export  const Product = mongoose.model('Product', ProductSchema, 'Product');
