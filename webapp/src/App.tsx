4b5761e69511aec527e00228934fa6f520623825
AnaOstio <anafdezostio01@gmail.com>
2022-03-07 14:33:32 +0100

    eliminar  imports no usados

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,4 @@
 import React, { useState, useEffect, FC } from 'react';
-/* import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Container from '@mui/material/Container';
-import EmailForm from './components/EmailForm';
-import Welcome from './components/Welcome';
-import UserList from './components/UserList';
-import  {getUsers} from './api/api';
-import {User} from './shared/shareddtypes'; */
 import 'bootswatch/dist/litera/bootstrap.min.css';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/Singup';
