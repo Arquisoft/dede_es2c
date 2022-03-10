2fabdf56cca0ef9c928327ed90d6463d8ce6fcb1
AnaOstio <anafdezostio01@gmail.com>
2022-03-10 09:32:01 +0100

    Intento de juntar front back

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,6 +1,24 @@
+import { Product } from '../shared/shareddtypes';
+import {getProducts} from '../api/api';
 import React, { useState, useEffect, FC } from 'react';
 
+
 const Home: FC = () => {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    const createProduct = async () => {
+        const pro: Product[] = await getProducts();
+        setProducts(pro);
+    }
+
+    useEffect(() => {
+        createProduct();
+
+        products.forEach(p => {
+            console.log(p.categoria);
+        });
+    })
 
     return (
         <div>
--- webapp/src/shared/shareddtypes.ts
+++ webapp/src/shared/shareddtypes.ts
@@ -1,4 +1,12 @@
 export type User = {
     name:string;
     email:string;
-  }
+}
+
+export type Product = {
+  codigo: string;
+  nombre: string;
+  categoria: string;
+  precio: string;
+  stock: string;
+}
