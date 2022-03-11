65aae6d9e7a554f6229b9477aed93ad21e4bbbd0
uo276255 <uo276255@uniovi.es>
2022-03-11 16:18:30 +0100

    Home acabado

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, FC } from 'react';
 import { Image } from '@mui/icons-material';
 import { Container } from '@mui/material';
-import img1 from '../img/MO01.jpg';
-import img2 from '../img/AL01.jpg';
-import img3 from '../img/RA01.jpg';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 const Home: FC = () => {
     return (
         <div className='Home'
@@ -13,13 +13,52 @@ const Home: FC = () => {
             alignItems: 'center',
             height: '80vh'
         }}>
-            <img src= {img1}/>
-            <img src= {img2}/>
-            <img src= {img3}/>
+        <Container sx={{ width: 1000, height: 400}}>
+            <ImageList variant="masonry" cols={3} gap={8}>
+                {imagesList.map((item) => (
+                <ImageListItem key={item.img}>
+                <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+             <ImageListItemBar position="bottom" title={item.description} />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Container>
             
             
         </div>
     )
-
 }
+
+const imagesList = [
+    {
+        img:'./img/MO01.jpg',
+        title:'display',
+        description: 'Display 200€',
+    },    
+    {
+        img: './img/AL01.jpg',
+        title:'speaker',
+        description: 'Speaker 15€',
+    },
+    {
+        img: './img/RA01.jpg',
+        title:'mouse',
+        description: 'Mouse 10€',
+    },
+    {
+        img:'./img/US01.jpg',
+        title:'usb',
+        description: 'Usb 15€',
+    },
+    {
+        img: './img/TE01.jpg',
+        title:'keyboard',
+        description: 'Keyboard 40€',
+    }
+];
 export default Home;
