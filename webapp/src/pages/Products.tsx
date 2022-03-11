6871b2e542e1bde261e0fd5ca86635a3356548e1
AdrianSantamarina <uo279062@uniovi.es>
2022-03-11 16:24:29 +0100

    Contenedor para los filtros de los productos

--- webapp/src/pages/Products.tsx
+++ webapp/src/pages/Products.tsx
@@ -1,10 +1,14 @@
 import React, { useState, useEffect, FC } from 'react';
+import Container from '@mui/material/Container';
+import { blue } from '@mui/material/colors';
 
 const Products: FC = () => {
 
     return (
         <div>
-
+            <Container component= "main" sx={{width: 100, height: 30, color: blue}}>
+                    
+            </Container>
         </div>
     )
 
