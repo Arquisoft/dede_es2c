64b8216f726dcb5bea145c49e4cc0d7110b3ba5d
uo278290 <uo278290@uniovi.es>
2022-03-29 21:59:46 +0200

    campo rol eliminado del modelo del usuario

--- restapi/src/model/User.ts
+++ restapi/src/model/User.ts
@@ -10,9 +10,6 @@ const UserSchema = new Schema({
     email: { 
         type: String, required: true, unique: true
     },
-    role: { 
-        type: String, required: true 
-    },
     password: { 
         type: String, required: true 
     }
