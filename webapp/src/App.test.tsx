3092fe65bb3653d733f10f2f3a95af6762f64403
AnaOstio <anafdezostio01@gmail.com>
2022-04-03 15:12:47 +0200

    Cambios test

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -21,7 +21,7 @@ test('Home Page de la aplicacion', async () => {
     const productList:Product[] = [{codigo: "TE01", categoria: "teclado", nombre: "PruebaTecado", precio: 20, stock: '4', url: "aa", descripcion: "", cantidad: 0}];
     const productOnCart:any = {};
     render(<Home onAddCart={productOnCart} cartItems = {productList}/>);
-    const linkElement = screen.getByAltText(/Banner/i);
+    const linkElement = screen.getByAltText(/Banner/i); 
     expect(linkElement).toBeInTheDocument();
     expect(screen.getByText(/Últimas unidades/i)).toBeInTheDocument(); 
 });
