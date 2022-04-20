9149f4abfdc5db68572003d94af85298cfa35e84
uo277189 <uo277189@uniovi.es>
2022-04-20 17:36:08 +0200

    Eliminación del random

--- restapi/tests/user.test.ts
+++ restapi/tests/user.test.ts
@@ -11,8 +11,7 @@ function makeid(length: any) {
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
+    result += characters.charAt(Math.floor(charactersLength));
  }
  return result;
 }
