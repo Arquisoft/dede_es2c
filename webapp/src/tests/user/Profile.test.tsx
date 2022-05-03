b8eea31b1adcdfc335651b8dd65f6ebad00a383e
AdrianSantamarina <uo279062@uniovi.es>
2022-05-03 19:42:23 +0200

    Arreglo para app.tsx y test relacionados con perfil, ya no llaman atributos

--- webapp/src/tests/user/Profile.test.tsx
+++ webapp/src/tests/user/Profile.test.tsx
@@ -5,7 +5,7 @@ import Profile from '../../pages/user/Profile';
 
 test('Carga correcta de la ventana SingUp',async () => {
      render(
-         <Profile email={"khdkadhka"} /> 
+         <Profile /> 
     );
 
     expect(screen.getByText(/No hay sesión inciada, por favor inicie sesión/i)).toBeInTheDocument();
